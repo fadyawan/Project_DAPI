6ee97d30fd343f184095e275e2c1cc01be8fa319
fadyawan <105607291+fadyawan@users.noreply.github.com>
2022-05-24 10:59:38 +0100

    Update PrimeNumbers_V2.py
    Wrong comment fixed.

--- PrimeNumbers_V2.py
+++ PrimeNumbers_V2.py
@@ -4,6 +4,6 @@ for x in range (0, 101):                    # To iterate through every single nu
         for y in range(2, x):               # Setting a divisor from two, because dividing by 0 is 0, and dividing by 1 doesn't return any of the values.
             if (x % y) == 0:                # Modulus function to check the remainder of the division, if it is 0, that means it is divisible by a number other than itself or one.
                 break                       # Leaving the if statement and iterating to the next x value.
-        else:                               #For, Else is a thing, If it doesn't break it goes to Else, if it breaks it iterates through to the first for loop.
+        else:                               # Else statement for when the conditions of a not prime number are not met, therefore we know it is a prime number.
             print(x)                        # Printing x
  
